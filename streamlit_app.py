9d9057712fc54e75870cbddc8ce17d1758a6d733
Vijaykokku <118242296+Vijaykokku@users.noreply.github.com>
2023-05-12 13:03:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 & Blueberryoatmeal')
-streamlit.text('Kale,Spinach & Rocket smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberryoatmeal')
+streamlit.text('🥗 Kale,Spinach & Rocket smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avakado Toast')
