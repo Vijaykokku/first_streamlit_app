c889c3ec844a188d6f2adb7e993a28ffd8e23a4d
Vijaykokku <118242296+Vijaykokku@users.noreply.github.com>
2023-05-16 11:04:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
- 
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 def get_fruityvice_data(this_fruit_choice):
@@ -40,9 +38,6 @@ except URLError as e:
   streamlit.error()
 streamlit.header('View Our Fruit List - Add Your Favourites!')
 
- 
-
-
 def get_fruit_load_list():
    with my_cnx.cursor() as my_cur:
       my_cur.execute("SELECT * from fruit_load_list")
@@ -52,8 +47,6 @@ if streamlit.button('Get Fruit Load List'):
    my_data_rows = get_fruit_load_list()
    my_cnx.close()
    streamlit.dataframe(my_data_rows)
-
- 
 
 def insert_row_snowflake(new_fruit):
    with my_cnx.cursor() as my_cur:
@@ -65,7 +58,6 @@ if streamlit.button('Add a Fruit to the  List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    back_from_function = insert_row_snowflake(add_my_fruit)
    streamlit.text(back_from_function)
-
 
 def insert_row_snowfalke(new_fruit):
     with my_cnx.cursor() as my_cur:
